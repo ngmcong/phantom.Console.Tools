058bdab9183edff20627f435214c6674c5e31f82
ngmcong <ngmcong@outlook.com>
2025-04-04 09:31:16 +0700

    *

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Any(x => x == "-re"))
+        {
+            var aParams = args.Skip(args.ToList().IndexOf("-re") + 1).Take(2);
+            if (aParams.Count() < 2)
+            {
+                System.Console.WriteLine("Error: -re requires two parameters.");
+                return;
+            }
+            var first = aParams.ElementAt(0);
+            var dir = first.Substring(0, first.LastIndexOf('/'));
+            var searchPattern = first.Replace(dir + "/", "");
+            var files = System.IO.Directory.GetFiles(dir, searchPattern);
+            var orgExt = searchPattern.TrimStart('*');
+            var ext = aParams.ElementAt(1);
+            foreach (string item in files)
+            {
+                File.Move(item, item.Replace(orgExt, $".{ext}"));
+            }
+        }
+    }
+}
