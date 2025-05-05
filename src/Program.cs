cd0208850e4d0a70035239812f16a3df1a98530c
ngmcong <ngmcong@outlook.com>
2025-05-05 18:13:35 +0700

    *

--- src/Program.cs
+++ src/Program.cs
@@ -3,11 +3,14 @@
 */
 
 using System;
-using System.Linq;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         if (args.Any(x => x == "-re"))
         {
@@ -28,5 +31,67 @@ class Program
                 File.Move(item, item.Replace(orgExt, $".{ext}"));
             }
         }
+        //dotnet run -apc "D:\Projects\InfomedHIS"
+        else if (args.Any(x => x == "-apc"))
+        {
+            var dirLocation = args[1];
+            Console.WriteLine(dirLocation);
+            HttpClient client = new HttpClient();
+            // Create the base64 encoded credentials
+            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes($"ngmcong:"));
+            // Set the Authorization header
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+            var response = await client.GetAsync("https://dev.azure.com/ngmcong/InfomedHIS/_apis/git/repositories?api-version=7.1");
+            // Check if the request was successful
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                var responseValue = JsonSerializer.Deserialize<AzureRepositories>(responseContent);
+                Console.WriteLine($"Request successful: {responseValue?.value?.Count()} items.");
+                if (responseValue == null) return;
+                foreach (var item in responseValue!.value!)
+                {
+                    var folderPath = Path.Combine(dirLocation, item.name!);
+                    if (Directory.Exists(folderPath))
+                    {
+                        Console.WriteLine($"Folder {folderPath} exists");
+                        continue;
+                    }
+                    var command = $"git clone {item.webUrl!}";
+                    Console.WriteLine(command);
+                    ProcessStartInfo psi = new ProcessStartInfo("cmd.exe");
+                    psi.WorkingDirectory = dirLocation;
+                    psi.Arguments = $"/c {command}";
+                    psi.UseShellExecute = false;
+                    psi.RedirectStandardOutput = true;
+                    psi.RedirectStandardError = true; // Capture error output as well
+                    psi.CreateNoWindow = true;       // Optional: Hide the cmd window
+                    var process = new Process()!;
+                    process.StartInfo = psi;
+                    process.Start();
+                    string error = await process.StandardError.ReadToEndAsync();
+                    await process.WaitForExitAsync();
+                    Console.WriteLine($"{error}");
+                    Console.WriteLine($"Git clone {item.name!} with exit code: {process.ExitCode}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Request failed with status code: {response.StatusCode}");
+                string errorContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error details: {errorContent}");
+            }
+        }
     }
+}
+public class AzureRepositories
+{
+    public IEnumerable<AzureRepository>? value { get; set; }
+}
+public class AzureRepository
+{
+    public string? id { get; set; }
+    public string? name { get; set; }
+    public string? url { get; set; }
+    public string? webUrl { get; set; }
 }
